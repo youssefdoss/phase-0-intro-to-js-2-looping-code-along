3ee716daa908d925a72891089a6334e69d33c848
Youssef Doss <youssefdoss97@gmail.com>
2022-08-06 22:52:12 +0200

    commit tree 8706a8a5d0dcefd1d0effe8ea170f53d666e8284

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const newArr = [];
+    for (let i = 0; i < names.length; i++) {
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArr;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
